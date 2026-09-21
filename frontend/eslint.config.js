56b8c62090eb5add09c0f4c95a0e8ca25fc19bde
Kelly883 <Kelly883@users.noreply.github.com>
2026-09-21 14:34:10 +0100

    Update ESLint config to ESM format with React plugin

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,15 +1,31 @@
-const js = require('@eslint/js');
-const next = require('eslint-config-next');
+import jsConfig from '@eslint/js';
+import reactPlugin from 'eslint-plugin-react';
+import nextConfig from 'eslint-config-next';
 
-/** @type {import('eslint').Linter.Config[]} */
-module.exports = [
-  js.configs.recommended,
-  ...next.configs.recommended,
+const { plugins, configs: nextConfigs } = nextConfig;
+
+export default [
+  jsConfig.configs.recommended,
+  ...nextConfigs.recommended,
   {
     files: ['**/*.{js,mjs,cjs,jsx,ts,mts,cts,tsx}'],
     rules: {
       'no-unused-vars': 'warn',
       'react/no-unescaped-entities': 'off',
+    },
+  },
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    plugins: {
+      react: reactPlugin,
+    },
+    languageOptions: {
+      ...nextConfig.languageOptions,
+      parserOptions: {
+        ...nextConfig.languageOptions.parserOptions,
+        ecmaVersion: 2022,
+        sourceType: 'module',
+      },
     },
   },
 ];
